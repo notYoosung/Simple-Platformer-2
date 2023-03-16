5cc7fc64cb5263e872c512089235fa6a6adfb635
notYoosung <notYoosung4901@gmail.com>
2023-03-16 14:35:25 -0500

    Updated player movement

--- Player.gd
+++ Player.gd
@@ -3,11 +3,28 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 	
 func _physics_process(delta: float) -> void:
-	velocity.y += 2
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = 10
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -10
+	apply_gravity()
+	
+	var input = Vector2.ZERO
+	input.x = Input.get_action_strength('ui_right') - Input.get_action_strength('ui_left')
+	
+	if input.x == 0:
+		apply_friction()
 	else:
-		velocity.x = 0
+		apply_acceleration(input.x)
+	
+	# player jump
+	if Input.is_action_just_pressed('ui_up'):
+		velocity.y = -100
+	
+	# apply movement to the player
 	velocity = move_and_slide(velocity)
+
+func apply_gravity():
+	velocity.y += 4
+
+func apply_friction():
+	velocity.x = move_toward(velocity.x, 0, 20)
+
+func apply_acceleration(amount):
+	velocity.x = move_toward(velocity.x, 50 * amount, 20)
