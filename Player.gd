8a58d3e56e072ae16dbbbf082650432f0a40401a
notYoosung <notYoosung4901@gmail.com>
2023-04-05 14:35:08 -0500

    Updated player sprite

--- Player.gd
+++ Player.gd
@@ -10,17 +10,35 @@ export(int) var ADDITIONAL_FALL_GRAVITY = 4
 
 var velocity = Vector2.ZERO
 
+onready var animatedSprite = $AnimatedSprite
+
+func _ready():
+	pass#animatedSprite.frames = "res://blue_skin.tres"
+
 func _physics_process(delta: float) -> void:
 	
 	apply_gravity()
 	var input = Vector2.ZERO
 	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
+	
+	
 	if input.x == 0:
 		apply_friction()
+		animatedSprite.animation = "Idle"
 	else:
 		apply_acceleration(input.x)
+		animatedSprite.animation = "Run"
+		if input.x > 0:
+			animatedSprite.flip_h = true
+		elif input.x < 0:
+			animatedSprite.flip_h = false
 		
+	if velocity.y > 0 and not is_on_floor():
+		animatedSprite.flip_v = true
+	else:
+		animatedSprite.flip_v = false
+	
 	# player jump
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
@@ -31,10 +49,17 @@ func _physics_process(delta: float) -> void:
 		
 		if velocity.y > 0:
 			velocity.y += ADDITIONAL_FALL_GRAVITY
+		animatedSprite.animation = "Jump"
 	
 	
+	var was_in_air = not is_on_floor()
 	# apply movement to the player
 	velocity = move_and_slide(velocity, Vector2.UP)
+	var just_landed = is_on_floor() and was_in_air
+	if just_landed:
+		animatedSprite.animation = "Run"
+		animatedSprite.frame = 1
+	
 
 func apply_gravity():
 	# gravity
